91f35c94d7acf4342339c4aa44c4d54ac69ca0da
ChrisKLoveless <chriskloveless@gmail.com>
2022-12-20 10:13:09 -0800

    add base project structure
    co-authored-by: Teddy Atkinson <tedddy.atkinson@gmail.com>

---
+++ Shelter/Models/ShelterContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Shelter.Models
+{
+  public class ShelterContext : DbContext
+  {
+    public  DbSet<Animal> Animals { get; set; }
+    public ShelterContext(DbContextOptions options) : base(options) { }
+  }
+}
